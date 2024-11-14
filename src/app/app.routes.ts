c0c34340838cb41b54af6de1542f0890cfe299ab
Cesar <csr2453@outlook.es>
2024-11-14 17:11:28 -0400

    Primeros componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: ()=>import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./store/dashboard/dashboard.component')
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./store/dashboard/dashboard.component')
+      },
+      {
+        path: 'tables',
+        loadComponent: () => import('./store/dashboard/dashboard.component')
+      },
+      {
+        path:'',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:'dashboard'
+  }
+];
--- src/app/shared/components/layout/layout.component.spec.ts
+++ src/app/shared/components/layout/layout.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { LayoutComponent } from './layout.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuComponent]
+      imports: [LayoutComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
